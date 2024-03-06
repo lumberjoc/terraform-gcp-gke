f6fdb4a54b669f68a76b2f07fee0e40d7e8cc8b6
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-03-06 15:01:05 -0800

    formatting

--- 8-node-pools.tf
+++ 8-node-pools.tf
@@ -1,64 +1,64 @@
 resource "google_service_account" "kubernetes" {
-    account_id = "kubernetes"
+  account_id = "kubernetes"
 }
 
 resource "google_container_node_pool" "general" {
-  name = "general"
-  cluster = google_container_cluster.primary.id
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
   node_count = 1
 
   management {
-    auto_repair = true
+    auto_repair  = true
     auto_upgrade = true
   }
 
   node_config {
-    preemptible = false
+    preemptible  = false
     machine_type = "e2-small"
 
     labels = {
-        role = "general"
+      role = "general"
     }
 
     service_account = google_service_account.kubernetes.email
-    oauth_scopes = [ 
-        "https://www.googleapis.com/auth/cloud-platform"
-     ]
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
 
 resource "google_container_node_pool" "spot" {
-    name = "spot"
-    cluster = google_container_cluster.primary.id
+  name    = "spot"
+  cluster = google_container_cluster.primary.id
 
-    management {
-      auto_repair = true
-      auto_upgrade = true
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      team = "devops"
     }
 
-    autoscaling {
-      min_node_count = 0
-      max_node_count = 10
+    taint {
+      key    = "instance_type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
     }
 
-    node_config {
-      preemptible = true
-      machine_type = "e2-small"
-
-      labels = {
-        team = "devops"
-      }
-
-      taint {
-        key = "instance_type"
-        value = "spot"
-        effect = "NO_SCHEDULE"
-      }
-
-      service_account = google_service_account.kubernetes.email
-      oauth_scopes = [ 
-         "https://www.googleapis.com/auth/cloud-platform"
-       ]
-    }
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 
 }
